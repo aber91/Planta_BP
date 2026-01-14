ca8a51edb70a0addd146a13c6b54f0a320feac2b
aber91 <aber2991@gmail.com>
2026-01-14 13:52:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -938,7 +938,7 @@ with tab_dashboard:
             "motivos": []
         }
     
-    if parametro not in ["HC", "DQO"]:+
+    if parametro not in ["HC", "DQO"]:
             
     return resultado
     
